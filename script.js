a910029e654cf7ed62c35d22648f2738ded47dbc
leonardchung10 <leonardchung10@gmail.com>
2026-02-21 14:21:24 +0800

    trying to fix the mobile ui

---
+++ script.js
@@ -0,0 +1,9 @@
+const reveals = document.querySelectorAll('.reveals');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target
+        }
+    })
+})
